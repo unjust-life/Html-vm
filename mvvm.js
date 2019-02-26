bda8a4379c9fe00c2706e940d042b5db18db4340
lzh <lzh@lzhdeMacBook-Pro.local>
2019-02-26 11:35:46 +0800

    v0.12 新增订阅模式

--- mvvm.js
+++ mvvm.js
@@ -1,57 +1,19 @@
-(function () {
-    // ES6版本  使用defineProperty实现
-    // class $vm {
-    //     constructor(data) {
-    //         this.data = data
-    //     }
-    //     bindModel(this, prop, fn) {
-    //         // 元编程
-    //         try {
-    //             Object.defineProperty(obj, prop, {
-    //                 // 取值
-    //                 get: function () {
-    //                     return prop
-    //                 },
-    //                 // 存值
-    //                 set(newVal) {
-    //                     //如果有指定函数 执行所需的回调函数
-    //                     if (fn) {
-    //                         fn(newVal)
-    //                     } else {
-    //                         // 直接设置为value的方法
-    //                         // var tag_name = dom.tagName.toLowerCase()
-    //                         // if(tag_name === "input" || tag_name === "textarea" || tag_name === "select") {
-    //                         //     dom.val(newVal)
-    //                         // } else {
-    //                         //     dom.txt(newVal)
-    //                         // }
-    //                     }
-    //                 },
-    //                 configurable: true
-    //             })
-    //         } catch {
-    //             console.log('数据绑定失败')
-    //         }
-    //     }
-    // }
-})
-
-// 兼容IE6 7 8低版本浏览器的数据绑定函数
-// 数据绑定函数操作  因为暂时不能实现虚拟dom 所以所有涉及的操作都需要绑定函数来实现 通过观察data上的数据变化对应dom操作
+// 兼容IE8低版本浏览器的数据绑定函数
+// 为了不影响页面服务端渲染 对项目本身代码产生影响 最后还是放弃对dom进行整体的拦截和编译 所以涉及的操作都需要函数式编程来实现最大兼容 框架本身不会对原有代码产生影响
 // 构造函数的开始
 var $vm = function (obj) {
     try {
-        //写入定义的data属性
+        //写入传入的对象
         this.data = obj.data
-        // 把属性列入观察模式
         this.watch = obj.watch
-        // 执行初始化方法
         this.created = obj.created
         this.methods = obj.methods
-        this.created()
+        this.$$fn = {}
         $(function () {
+            // dom加载后进行初始化
+            this.created()
             this.methods()
-
+            // 设置初始值
             for (var key in this.data) {
                 //只遍历对象自身的属性，而不包含继承于原型链上的属性。  
                 if (this.data.hasOwnProperty(key) === true) {
@@ -60,46 +22,63 @@ var $vm = function (obj) {
                 }
             }
         }.bind(this))
-    } catch {
+    } catch(e) {
         console.log('new对象失败')
     }
 }
-// 新增绑定的数据 prop属性名 val初始值 fn回调函数
+// 新增绑定的数据 prop属性名 val初始值
 $vm.prototype.bindModel = function (prop, val) {
     try {
-        this.data[prop] = null
         this.data[prop] = val
-    } catch{
+    } catch(e){
         console.log(prop + '数据绑定失败')
     }
 }
-// 给dom快速绑定val的方法 dom指向要绑定的元素 prop是data上的属性 val是初始值
+// 给dom快速绑定val的方法 dom指向要绑定的元素 prop是需绑定的属性名 val是初始值
+// 
 $vm.prototype.bindVal = function (dom, prop, val) {
     try {
         var that = this
+        // 订阅对象 隐藏函数  如果订阅的值发生改变就会通知所有dom
+        that.$$fn[prop] = function (newVal) {
+            dom.each(function () {
+                var $this = $(this)
+                var tag_name = $(this)[0].tagName.toLowerCase()
+                if(tag_name === "input" || tag_name === "textarea" || tag_name === "select") {
+                    $this.val(newVal)
+                } else {
+                    $this.text(newVal)
+                }
+            })
+        }
         // 获得标签类型
-        // var tag_name = dom.tagName.toLowerCase()
-        // if(tag_name === "input" || tag_name === "textarea" || tag_name === "select") {
-            dom.val(val)
-        // } else {
-            // dom.txt(val)
-        // }
-        // 执行双绑 监听dom的变化重置data数据
-        dom.on("input propertychange", function (e) {
-            var newVal = dom.val()
-            that.set(prop, newVal)
-          })
-    } catch {
+        dom.each(function () {
+            var $this = $(this)
+            var tag_name = $(this)[0].tagName.toLowerCase()
+            // 如果是合法类型就赋值并监听数据实时变化
+            if(tag_name === "input" || tag_name === "textarea" || tag_name === "select") {
+                $this.val(val)
+                // 执行双绑 监听dom的变化重置data数据
+                $this.on("input propertychange", function (e) {
+                    var newVal = $this.val()
+                    that.set(prop, newVal)
+                })
+                //如果不是则进行写入文本
+            } else {
+                $this.text(val)
+            }
+        })
+    } catch(e) {
         console.log(prop, '数据双绑失败')
     }
 }
 // 取值
 $vm.prototype.get = function (prop) {
     try {
-        console.log(prop + '值为' + this.data[prop])
+        // console.log(prop + '值为' + this.data[prop])
         //返回当前值
         return this.data[prop]
-    } catch {
+    } catch(e) {
         console.log(prop + '取值失败')
     }
 }
@@ -123,7 +102,11 @@ $vm.prototype.set = function (prop, val) {
         if (this.watch[prop]) {
             this.watch[prop](val)
         }
-    } catch {
+        //查询是否有订阅值
+        if (this.$$fn[prop]) {
+            this.$$fn[prop](val)
+        }
+    } catch(e) {
         console.log(prop + '存值失败')
     }
 }
